31af41c36121b83d413d50abebcca56723523f8a
cherrytrees-kpu <michael.ke200@gmail.com>
2022-05-25 20:18:34 +0000

    first working version of multiprocess_blast

--- src/probedesign/multiprocess_blast.py
+++ src/probedesign/multiprocess_blast.py
@@ -6,22 +6,34 @@ import pandas as pd
 
 blastdb = '/data/db/SSU-nematodes.fasta'
 blastdb_len = 16094
-oligos = {
+test_oligos = {
     '1630-17':'CAGAAGAGATCAACCTCGTTA',
     '1631-18':'AGAAGAGATCAACCTCGTT',
     '1640-19':'GAAGAGATCAACCTCGTT',
     '1641-20':'AAGAGATCAACCTCGTTA',
 }
+test_oligos2 = [
+    [('1630-17','CAGAAGAGATCAACCTCGTTA'),
+    ('1630-18','CAGAAGAGATCAACCTCGTTA'),],
+    [('1631-19','CAGAAGAGATCAACCTCGTTA'),
+    ('1631-20','CAGAAGAGATCAACCTCGTTA'),],
+    [('1631-21','CAGAAGAGATCAACCTCGTTA'),
+    ('1631-22','CAGAAGAGATCAACCTCGTTA'),],
+    [('1631-23','CAGAAGAGATCAACCTCGTTA'),
+    ('1631-24','CAGAAGAGATCAACCTCGTTA'),],
+]
 
 
 def blast_all(oligos): 
     #Generate the oligo temporary file
     fasta = tempfile.NamedTemporaryFile(delete=True)
-    print('Oligos in the temp file: ')
+    #print(fasta)
+    print(oligos)
+    print(type(oligos))
     for oligo in oligos:
-        fasta.write(f">{str(oligo.id)}\n{str(oligo.seq)}\n".encode())
-        print(oligo.id)
-        print(oligo.seq)
+        print(f">{oligo[0]}\n{oligo[1]}\n")
+        fasta.write(f">{oligo[0]}\n{oligo[1]}\n".encode())
+        #fasta.write(f">{oligo}\n{oligos[oligo]}\n".encode())
     fasta.seek(0)
     #cpu_count = multiprocessing.cpu_count() - 2
     cpu_count = 4
@@ -66,6 +78,7 @@ def blast_all(oligos):
     ]
     data = pd.read_csv(output, sep=',', header=None, names=headers)
     fasta.close()
+    #print(data)
     #Split the data
     list_oligo_ids = set(data['qacc'])
     #print(f"qacc set:\n{list_oligo_ids}")
@@ -75,9 +88,13 @@ def blast_all(oligos):
     return blast_results
 
 def multiblast(oligos): 
-    pool = multiprocessing.pool(4)
+    #print(oligos)
+    pool = multiprocessing.Pool(4)
     result = pool.map(blast_all, oligos)
-    print(len(result))
+    print(result)
+    for item in result: 
+        print(item)
 
 if __name__ == "__main__":
-    multiblast(oligos)
+    # multiblast(test_oligos)
+    multiblast(test_oligos2)
